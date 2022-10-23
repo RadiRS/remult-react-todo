352a2bdb16041da70d0e251e0485b9cbec7eb62a
Radi Rusadi <radi.rusadi.s@gmail.com>
2022-10-23 21:33:02 +0700

    Added toggle display of completed task request

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,40 @@ import { Task } from './shared/task.model'
 
 const taskRepo = remult.repo(Task);
 
-async function fetchTasks() {
-  return taskRepo.find()
+async function fetchTasks(completed?: boolean) {
+  return taskRepo.find({
+    limit: 20,
+    orderBy: { completed: 'asc' },
+    where: { completed: completed ? false : undefined }
+  })
 }
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([])
+  const [hideCompleted, setHideCompleted] = useState<boolean>(false)
 
   useEffect(() => {
-    fetchTasks().then(setTasks)
-  }, [])
+    fetchTasks(hideCompleted).then(setTasks)
+  }, [hideCompleted])
 
   return (
     <div>
+      <input
+        type="checkbox"
+        checked={hideCompleted}
+        onChange={(e) => {
+          setHideCompleted(e.target.checked)
+        }}
+      />
+      Hide Completed
       <main>
         <h2>Todo List</h2>
         {tasks.map(task => (
           <div key={task.id}>
-            <input type="checkbox" checked={task.completed} />
+            <input type="checkbox"
+              checked={task.completed}
+              onChange={e => console.log(e.target.checked)}
+            />
             {task.title}
           </div>
         ))}
