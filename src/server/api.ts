d00cd8185f5133c128c8a50dacb908097615e1dd
Radi Rusadi <radi.rusadi.s@gmail.com>
2022-10-23 20:56:47 +0700

    Added task-model and set test data api

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,3 +1,19 @@
+import { remult } from 'remult';
 import { remultExpress } from 'remult/remult-express';
+import { Task } from '../shared/task.model';
 
-export const api = remultExpress();
+export const api = remultExpress({
+  entities: [Task],
+  initApi: async () => {
+    const taskRepo = remult.repo(Task);
+    if (await taskRepo.count() === 0) {
+      await taskRepo.insert([
+        { title: "Task a" },
+        { title: "Task b", completed: true },
+        { title: "Task c" },
+        { title: "Task d" },
+        { title: "Task e", completed: true }
+      ]);
+    }
+  }
+});
---
+++ src/shared/task.model.ts
@@ -0,0 +1,16 @@
+import { Entity, Fields } from 'remult'
+
+@Entity("tasks", {
+  allowApiCrud: true
+})
+
+export class Task {
+  @Fields.uuid()
+  id!: string;
+
+  @Fields.string()
+  title = '';
+
+  @Fields.boolean()
+  completed = false;
+}
